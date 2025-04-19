0b9bc148e5a7da17ac64a098301d0337b9c8e213
Abouzeid97 <sherif.abouzeid97@gmail.com>
2025-04-19 18:32:41 +0200

    adding seleniumScraper.py without API

---
+++ SeleniumScraper.py
@@ -0,0 +1,69 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+import time
+
+# Setup Selenium WebDriver
+driver = webdriver.Chrome()
+# Ask user for search input
+search_term = input("Enter product to search: ")
+url = f"https://www.amazon.eg/s?k={search_term}&language=en_AE"
+driver.maximize_window()
+
+driver.get(url)
+if "503" in driver.page_source:
+    time.sleep(6)
+    link = driver.find_element(By.XPATH, '//a[@href="https://www.amazon.eg/ref=cs_503_link/"]')
+    original_href = link.get_attribute("href")
+    actions = ActionChains(driver)
+    actions.move_to_element(link).click().perform()
+    time.sleep(6)
+    new_href = original_href + "?language=en_AE"
+    driver.get(new_href)
+    time.sleep(5)
+    search_box = driver.find_element(By.ID, "twotabsearchtextbox")
+    # Click the search box to focus it
+    search_box.click()
+    # Type the product name (e.g., "laptop") and hit Enter
+    search_box.send_keys("laptop")
+    search_box.send_keys(Keys.RETURN)
+
+last_page_element = driver.find_element(By.XPATH, '//span[@class="s-pagination-item s-pagination-disabled"]')
+
+# Get the text content and convert to int
+last_page_number = int(last_page_element.text)
+
+print("Last page number:", last_page_number)
+time.sleep(10)
+
+for i in range(last_page_number):
+    # Get all product elements    
+    print(f"page {i}")
+    products = driver.find_elements(By.XPATH, '//div[@role="listitem"]')
+    
+    print(f"\nFound {len(products)} products with role='listitem'\n")
+    
+    # Loop through and extract titles + hrefs
+    for product in products:
+        try:
+            title_elem = product.find_element(By.XPATH, ".//h2//span")
+            link_elem = product.find_element(By.XPATH, ".//a[@class='a-link-normal s-line-clamp-4 s-link-style a-text-normal']")
+            rating_elem = product.find_element(By.XPATH, ".//span[@class='a-icon-alt']")
+            rating_text = rating_elem.get_attribute("textContent").strip()
+            price_elem = product.find_element(By.XPATH, ".//span[@class='a-price-whole']")
+            print("Title:", title_elem.text)
+            print("Link:", "https://www.amazon.eg" + link_elem.get_attribute("href"))
+            print("Rating:",rating_text)
+            print("Price:", price_elem.text, "EGP")
+            print("------")
+        except Exception as e:
+            print("this is not a product")
+    if i != last_page_number-1:
+        next_button = driver.find_element(By.XPATH, '//a[contains(@aria-label, "Go to next page")]')
+        next_button.click()
+        time.sleep(5)
+
+    
+time.sleep(60)
+driver.quit()
